f64736d190a63e811f7e540630d09d002e22d2f8
jaumard <jimmy.aumard@gmail.com>
2022-06-15 07:14:43 +0200

    migrate to Flutter 3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.1] - 15/06/2022
+
+- migrate to Flutter 3.0
+
 ## [0.1.0] - 18/04/2021
 
 - migrate to null safety
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.subtitle1,
             ),
           ],
         ),
